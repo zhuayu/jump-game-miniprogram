a0e33a03627c3d66af238e3d5ab3e1335d7e384a
Jax Chu <294242589@qq.com>
2021-02-10 17:04:45 +0800

    feat: init

---
+++ game.js
@@ -0,0 +1,9 @@
+import './js/libs/weapp-adapter'
+import './js/libs/symbol'
+import * as Three from './js/libs/three.min.r96.js'
+
+console.log(Three)
+
+// import Main from './js/main'
+
+// new Main()
